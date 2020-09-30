18e2bbaa57ca5b038f02cde406d75ff6af0cc8cd
Lhoussaine <bouaik.lhou@gmail.com>
2020-09-30 23:32:39 +0100

    Implement Display component

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import PropTypes from "prop-types";
 
-function Display() {
+function Display({ result }) {
     return (
         <div>
-
+            <h4>{result}</h4>
         </div>
     )
 }
+
+Display.PropTypes = {
+    result: PropTypes.string,
+};
+Display.defaultProps = {
+    result: '0',
+};
 
 export default Display
