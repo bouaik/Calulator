da4024bcd9f18b8ce6f32de59f774b243b0956de
Lhoussaine <bouaik.lhou@gmail.com>
2020-10-01 22:40:55 +0100

    Add style to buttonpanel component

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -4,31 +4,31 @@ import Button from './Button';
 function ButtonPanel() {
   return (
     <div className="button-panel">
-      <div className="group1">
+      <div className="group">
         <Button name="AC" />
         <Button name="+/-" />
         <Button name="%" />
         <Button name="÷" />
       </div>
-      <div className="group2">
+      <div className="group">
         <Button name="7" />
         <Button name="8" />
         <Button name="9" />
         <Button name="X" />
       </div>
-      <div className="group3">
+      <div className="group">
         <Button name="4" />
         <Button name="5" />
         <Button name="6" />
         <Button name="-" />
       </div>
-      <div className="group4">
+      <div className="group">
         <Button name="1" />
         <Button name="2" />
         <Button name="3" />
         <Button name="+" />
       </div>
-      <div className="group5">
+      <div className="group">
         <Button name="0" />
         <Button name="." />
         <Button name="=" />
