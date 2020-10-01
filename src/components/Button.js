2f1a2c30f0c255cb402062efb384124e75d0c29d
Lhoussaine <bouaik.lhou@gmail.com>
2020-10-01 22:25:55 +0100

    Make proptype name required

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,15 +2,15 @@ import React from 'react';
 import propTypes from 'prop-types';
 
 function Button({ name }) {
-  return (
-    <div>
-      <button type="button">{name}</button>
-    </div>
-  );
+    return (
+        <div>
+            <button type="button">{name}</button>
+        </div>
+    );
 }
 
 Button.propTypes = {
-    name: propTypes.string, // eslint-disable-line
+    name: propTypes.string.isRequired,
 };
 
 export default Button;
