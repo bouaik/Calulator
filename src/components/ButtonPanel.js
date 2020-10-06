d7d31b5cf5a165a3f9a9afe291260986927eb848
Lhoussaine <bouaik.lhou@gmail.com>
2020-10-06 03:59:18 +0100

    Pass clickhandler to each button in the panel

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -5,33 +5,33 @@ function ButtonPanel() {
   return (
     <div className="button-panel">
       <div className="group">
-        <Button name="AC" color="#dfdfdf" />
-        <Button name="+/-" color="#dfdfdf" />
-        <Button name="%" color="#dfdfdf" />
-        <Button name="÷" />
+        <Button name="AC" color="#dfdfdf" clickHandler={clickHandler} />
+        <Button name="+/-" color="#dfdfdf" clickHandler={clickHandler} />
+        <Button name="%" color="#dfdfdf" clickHandler={clickHandler} />
+        <Button name="÷" clickHandler={clickHandler} />
       </div>
       <div className="group">
-        <Button name="7" color="#dfdfdf" />
-        <Button name="8" color="#dfdfdf" />
-        <Button name="9" color="#dfdfdf" />
-        <Button name="x" />
+        <Button name="7" color="#dfdfdf" clickHandler={clickHandler} />
+        <Button name="8" color="#dfdfdf" clickHandler={clickHandler} />
+        <Button name="9" color="#dfdfdf" clickHandler={clickHandler} />
+        <Button name="x" clickHandler={clickHandler} />
       </div>
       <div className="group">
-        <Button name="4" color="#dfdfdf" />
-        <Button name="5" color="#dfdfdf" />
-        <Button name="6" color="#dfdfdf" />
-        <Button name="-" />
+        <Button name="4" color="#dfdfdf" clickHandler={clickHandler} />
+        <Button name="5" color="#dfdfdf" clickHandler={clickHandler} />
+        <Button name="6" color="#dfdfdf" clickHandler={clickHandler} />
+        <Button name="-" clickHandler={clickHandler} />
       </div>
       <div className="group">
-        <Button name="1" color="#dfdfdf" />
-        <Button name="2" color="#dfdfdf" />
-        <Button name="3" color="#dfdfdf" />
-        <Button name="+" />
+        <Button name="1" color="#dfdfdf" clickHandler={clickHandler} />
+        <Button name="2" color="#dfdfdf" clickHandler={clickHandler} />
+        <Button name="3" color="#dfdfdf" clickHandler={clickHandler} />
+        <Button name="+" clickHandler={clickHandler} />
       </div>
       <div className="group">
-        <Button name="0" color="#dfdfdf" wide />
-        <Button name="." color="#dfdfdf" />
-        <Button name="=" />
+        <Button name="0" color="#dfdfdf" clickHandler={clickHandler} wide />
+        <Button name="." color="#dfdfdf" clickHandler={clickHandler} />
+        <Button name="=" clickHandler={clickHandler} />
       </div>
     </div>
   );
