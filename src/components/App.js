0c80fa6f7b39f97a950e2dd5776ee4d35cb7b85c
Lhoussaine <bouaik.lhou@gmail.com>
2020-10-06 03:43:23 +0100

    Change func comp to class comp

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import ButtonPanel from './ButtonPanel';
 import Display from './Display';
 import calculate from '../logic/calculate'; // eslint-disable-line
 
-function App() {
-  return (
-    <div className="container">
-      <div className="App">
-        <h1 className="title">Calculator</h1>
-        <Display />
-        <ButtonPanel />
+class App extends Component {
+  render() {
+    return (
+      <div className="container" >
+        <div className="App">
+          <h1 className="title">Calculator</h1>
+          <Display />
+          <ButtonPanel />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
